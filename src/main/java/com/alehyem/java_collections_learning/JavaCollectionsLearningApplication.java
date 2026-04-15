bc23d51179d3103b226322daebbf800d806f596e
Aleh_Yemelyanchyk <yem1403@gmail.com>
2026-04-15 19:01:05 +0500

    Initial commit

---
+++ src/main/java/com/alehyem/java_collections_learning/JavaCollectionsLearningApplication.java
@@ -0,0 +1,13 @@
+package com.alehyem.java_collections_learning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaCollectionsLearningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaCollectionsLearningApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/alehyem/java_collections_learning/JavaCollectionsLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alehyem.java_collections_learning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaCollectionsLearningApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
